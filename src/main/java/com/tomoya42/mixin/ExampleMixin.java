eb1bd9820d2b4266f12bbb09f89f870e85adf080
Tomoya <py2qiuse@gmail.com>
2023-07-26 15:23:11 +0800

    init

---
+++ src/main/java/com/tomoya42/mixin/ExampleMixin.java
@@ -0,0 +1,36 @@
+package com.tomoya42.mixin;
+
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
+import net.minecraft.entity.Entity;
+import net.minecraft.item.EnchantedBookItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.village.TradeOffer;
+import net.minecraft.village.TradeOffers;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Mixin(TradeOffers.EnchantBookFactory.class)
+public class ExampleMixin {
+    @Inject(at = @At("RETURN"), method = "create", cancellable = true)
+    private void init(Entity entity, Random random, CallbackInfoReturnable<TradeOffer> cir) {
+        List<Enchantment> list = Registries.ENCHANTMENT.stream().filter(Enchantment::isAvailableForEnchantedBookOffer).collect(Collectors.toList());
+        Enchantment enchantment = list.get(random.nextInt(list.size()));
+        int i = enchantment.getMaxLevel();
+        ItemStack itemStack = EnchantedBookItem.forEnchantment(new EnchantmentLevelEntry(enchantment, i));
+        int j = 2 + 3 * i;
+        if (j > 64) j = 64;
+
+        TradeOffer returnValue = cir.getReturnValue();
+        TradeOffer tradeOffer = new TradeOffer(new ItemStack(Items.EMERALD, j), new ItemStack(Items.BOOK), itemStack, 12, returnValue.getMerchantExperience(), 0.2F);
+        cir.setReturnValue(tradeOffer);
+    }
+}

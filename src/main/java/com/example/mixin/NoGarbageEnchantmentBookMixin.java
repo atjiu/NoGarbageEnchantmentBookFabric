73b2d603d7ceca136209ab70d6101c25a219396c
Tomoya <py2qiuse@gmail.com>
2023-08-01 17:20:20 +0800

    update author

--- src/main/java/com/example/mixin/NoGarbageEnchantmentBookMixin.java
+++ src/main/java/com/example/mixin/NoGarbageEnchantmentBookMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
-@Mixin(targets = "net/minecraft/village/TradeOffers$TypeAwareBuyForOneEmeraldFactory")
+@Mixin(targets = "net/minecraft/village/TradeOffers$EnchantBookFactory")
 public class NoGarbageEnchantmentBookMixin {
 
     @Inject(at = @At("RETURN"), method = "create", cancellable = true)
